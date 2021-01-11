165d00820c1844b0b874d8c88f2eb2798fb32e2b
Rozdandy <rozdandy@gmail.com>
2021-01-11 03:19:40 +0000

    Create the condition to flip a card

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,8 +67,7 @@ class ClimateChangeGame {
         this.cardToCheck = null;
         this.matchedCards = [];
         this.busy = true;
-        
-        setTimeout(() => {
+            setTimeout(() => {
             this.audioController.startMusic();
             this.shuffleCards(this.cardsArray);
             this.countdown = this.startCountdown();
@@ -79,38 +78,7 @@ class ClimateChangeGame {
         this.ticker.innerText = this.totalClicks;  
      }
 
-
-     startCountdown() {
-// the function that counts the time down
-        return setInterval(() => {
-            this.timeRemaining--;
-            this.timer.innerText = this.timeRemaining;
-            if(this.timeRemaining === 0)
-                this.gameOver();
-        }, 1000);
-    }
-
-   gameOver() {
-        clearInterval(this.countdown);
-        this.audioController.gameOver();
-        document.getElementById('game-over-text').classList.add('visible');
-    }
-
-
-    victory() {
-        clearInterval(this.countdown);
-        this.audioController.victory();
-        document.getElementById('victory-text').classList.add('visible');
-    }
-
-     hideCards() {
-        this.cardsArray.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.remove('matched');
-        });
-    }
-
-// this function flips the cards
+    // this function flips the cards
     flipCard(card) {
         if(this.canFlipCard(card)) {
             this.audioController.flip();
@@ -125,48 +93,8 @@ class ClimateChangeGame {
             }
         }
     }
-// for a pair of cards to match
-  checkForCardMatch(card) {
-        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
-            this.cardMatch(card, this.cardToCheck);
-        else 
-            this.cardMismatch(card, this.cardToCheck);
 
-        this.cardToCheck = null;
-    }
-
-// for a card that match
-    cardMatch(card1, card2) {
-        this.matchedCards.push(card1);
-        this.matchedCards.push(card2);
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-        this.audioController.match();
-        if(this.matchedCards.length === this.cardsArray.length)
-            this.victory();
-    }
-
-    cardMismatch(card1, card2) {
-        this.busy = true;
-        setTimeout(() => {
-            card1.classList.remove('visible');
-            card2.classList.remove('visible');
-            this.busy = false;
-        }, 1000);
-    }
-
-    shuffleCards(cardsArray) { // Fisher-Yates Shuffle Algorithm.
-        for (let i = cardsArray.length - 1; i > 0; i--) {
-            let randIndex = Math.floor(Math.random() * (i + 1));
-            cardsArray[randIndex].style.order = i;
-            cardsArray[i].style.order = randIndex;
-        }
-    }
-
-     getCardType(card) {
-        return card.getElementsByClassName('win-match')[0].src;
-    }
-// conditions if a card could be flipped
+    // conditions if a card could be flipped
      canFlipCard(card) {
         return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
     }
@@ -195,7 +123,7 @@ function ready() {
         });
     });
 
-// this function begins the game
+// this perform a function whenever a card is clicked
     cards.forEach(card => {card.addEventListener('click', () => {
           game.flipCard(card);
     });
